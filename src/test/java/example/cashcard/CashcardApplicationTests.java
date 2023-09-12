c10e76508631c34456d163a7d87aadf1138b7b6a
mdhenriques <md.henriques@outlook.com>
2023-09-12 15:42:36 -0300

    Adding DirtiesContext annotation to CashCardApplicationTests

--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -18,6 +18,7 @@ import static org.springframework.test.annotation.DirtiesContext.*;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 class CashCardApplicationTests {
     @Autowired
     TestRestTemplate restTemplate;
