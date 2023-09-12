72fec9594f9a9e90a1334100c70fc5ecfb2831ee
mdhenriques <md.henriques@outlook.com>
2023-09-12 15:13:16 -0300

    Creating PostMapping

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,6 +1,7 @@
 package example.cashcard;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,11 @@ public class CashCardController {
     public CashCardController(CashCardRepository cashCardRepository) {
         this.cashCardRepository = cashCardRepository;
     }    
+
+    @PostMapping
+    private ResponseEntity createCashCard() {
+        return null;
+    }
 
     @GetMapping("/{requestedId}")
     public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
