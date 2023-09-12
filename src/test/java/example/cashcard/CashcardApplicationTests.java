7f2720e07f8390ced7beb9f68926c7c401a540bf
mdhenriques <md.henriques@outlook.com>
2023-09-12 12:09:48 -0300

    Updating test. Checking if the Id return is not null

--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -21,5 +21,9 @@ class CashCardApplicationTests {
         ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+		Number id = documentContext.read("$.id");
+		assertThat(id).isNotNull();
     }
 }
