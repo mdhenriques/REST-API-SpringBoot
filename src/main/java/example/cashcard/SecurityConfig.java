1de4395132500a88bfd74ed0572cf458184233ff
mdhenriques <md.henriques@outlook.com>
2023-09-13 10:39:37 -0300

    Adding UserDetailsService

--- src/main/java/example/cashcard/SecurityConfig.java
+++ src/main/java/example/cashcard/SecurityConfig.java
@@ -26,6 +26,17 @@ public class SecurityConfig {
     }
 
     @Bean
+    public UserDetailsService testOnlyUsers(PasswordEncoder passwordEncoder) {
+        User.UserBuilder users = User.builder();
+        UserDetails sarah = users
+            .username("sarah1")
+            .password(passwordEncoder.encode("abc123"))
+            .roles() // No roles for now
+            .build();
+        return new InMemoryUserDetailsManager(sarah);
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
