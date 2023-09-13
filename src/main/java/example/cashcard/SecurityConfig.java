643ae1715d2366b46c3eb25020d5bfe9b8af1521
mdhenriques <md.henriques@outlook.com>
2023-09-13 10:55:14 -0300

    Editing security filter chain

--- src/main/java/example/cashcard/SecurityConfig.java
+++ src/main/java/example/cashcard/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
             .requestMatchers("/cashcards/**")
-            .authenticated()
+            .hasRole("CARD-OWNER")
             .and()
             .csrf().disable()
             .httpBasic();
