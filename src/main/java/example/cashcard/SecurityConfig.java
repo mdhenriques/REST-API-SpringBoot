6ddcaf247f1d5bdefb0b779ed8a922a8fe5447d7
mdhenriques <md.henriques@outlook.com>
2023-09-13 10:36:22 -0300

    Adding annotations to SecurityConfig

--- src/main/java/example/cashcard/SecurityConfig.java
+++ src/main/java/example/cashcard/SecurityConfig.java
@@ -11,8 +11,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.build();
     }
